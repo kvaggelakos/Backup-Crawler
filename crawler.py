e85748d4bde1616c4c77cfb510898c4fbdf9589d
Konstantinos Vaggelakos <kozze89@gmail.com>
2012-06-04 20:46:51 +0200

    Updated the crawler. Now using bs to try and find links

--- crawler.py
+++ crawler.py
@@ -6,15 +6,22 @@ import sys
 import re
 import urllib2
 import urlparse
+import os
+import httplib2
 from google import search
+from BeautifulSoup import BeautifulSoup, SoupStrainer
 
 
 # Global vars
 search_query = 'allinurl: %(search_term)s filetype:%(filetype)s'
-
+download_dir = 'downloaded/'
 
 # This is the main entry point for the application and will start searching immidiately after validating the input parameters
 def main():
+	# Check if there is a directory to store the files otherwise create it
+	if not os.path.exists(download_dir):
+		os.makedirs(download_dir)
+
 	# Check input parameters
 	if (len(sys.argv) != 4):
 		start_search('/administrator/backups', 'sql', 20)
@@ -30,57 +37,45 @@ def start_search(search_term, filetype, limit):
 	print_settings(search_term, filetype, limit)
 	# Start the search
 	for url in search(search_query % vars(), stop=limit):
-		print 'Digging into: %s' % url
-		handle_url(url)
+		handle_url(url, filetype)
 
 
 # This method will check for more backups in the same directory if directory listing is available
 # @url The url should be a url to an existing sql backup file
 def handle_url(url, filetype):
-	# Get the relative path and get the html to search for links to other backup files
-	links = download_html(url[0:url.rfind('/')])
-	if (links == None):
-		print 'There was a problem looking into that path, downloading file found by google'
-		if (download_file(url, 'a') == None): # Change name
-			print 'Couldn\'t even download the file pointed out by google'
-		else:
-			print 'Sucessfully downloaded: %s' % url
-	else:
-		# Go through all links and see if they are of sql file type
-		for link in links:
-			if (link.find('.%s' % filetype) == None):
-				print link + '%s was not of type: %s' % (link, filetype)
+	# Create directory to save dumps
+	base_dir = url[0:url.rfind('/')]
+	down_dir = download_dir + base_dir[7:base_dir.find('/',7)]
+
+	print 'Digging into: %s' % base_dir
+	if not os.path.exists(down_dir):
+		os.makedirs(down_dir)
+		print 'Created directory %s' % down_dir
+	
+	
+	# Get all links on this page
+	http = httplib2.Http()
+	status, response = http.request(base_dir)
+
+
+	for link in BeautifulSoup(response, parseOnlyThese=SoupStrainer('a')):
+		if (link.has_key('href')):
+			if (link['href'].find('.%s' % filetype) == None):
+				print link['href'] + '%s was not of type: %s' % (link['href'], filetype)
 			else:
-				if (download_file(link, 'a') == None): #TODO: Change name
-					print 'There was a problem in downloading the file: %s' % link
-				else:
-					print 'Downloaded file: %s\' successfully!' % link
-
-
-def download_html(url):
-	try:
-		return urllib2.urlopen(url).read()
-	except urllib2.URLError as e:
-		print 'There was an error trying to download the file: %s' % url
-		return None
+				if (download_file(link['href'], down_dir + link['href'][link['href'].rfind('/'):]) == None):
+					print 'There was a problem in downloading the file: %s' % link['href']
 
 
 def download_file(url, filename):
 	try:
+		print 'Downloading file: %s into %s' % (url, filename)
 		url_handle = urllib2.urlopen(url)
 		localFile = open('downloaded/%s' % filename, 'w')
 		localFile.write(url_handle.read())
 		localFile.close()
 	except:
-		print 'There was an error trying to download the file: %s' % url
 		return None
-
-
-def extract_urls(text):
-	url_re = re.compile(r'\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))')
-	for match in url_re.finditer(your_text):
-		yield match.group(0)
-
 
 
 # Prints the settings specified by the user
